4c9746963e87746fa16353e58cd0317351a79571
ALEJANDRO NAVARRO DE LA CRUZ <alejandro.navarro10@alu.uclm.es>
2022-12-09 17:19:28 +0100

    Tercer Commit TallerIPO

--- src/constants/objetos.ts
+++ src/constants/objetos.ts
@@ -1,23 +1,56 @@
+import { ImageSourcePropType } from "react-native"
+
 export interface objeto{
     marca:string
     modelo:string
-    tipo:string
-    imagen:string
+    tipo:string,
+    precio:number,
+    imagen:ImageSourcePropType
 }
 
 export const listaObjetos:Array<objeto> = [
     {
        marca:"RIDEX",
-       imagen:"URl",
+       imagen: require('../assets/objetos/filtro.png'),
        modelo:"Filtro S41",
-       tipo:"Enroscable" 
+       tipo:"Enroscable" ,
+       precio:19.99,
     },
     {
         marca:"Puerta Verde",
-        imagen:"URl",
+        imagen:require('../assets/objetos/puerta.png'),
         modelo:"C-29",
-        tipo:"Enroscable" 
-     },
+        tipo:"Enroscable" ,
+        precio:30.50,
+    },
+    {
+        marca:"Axion23",
+        imagen:require('../assets/objetos/montonRuedas.png'),
+        modelo:"Axion23",
+        tipo:"Invierno" ,
+        precio:40.19,
+    },
+    {
+        marca:"Axion23",
+        imagen:require('../assets/objetos/montonRuedas.png'),
+        modelo:"Axion23",
+        tipo:"Invierno" ,
+        precio:40.49,
+    },
+    {
+        marca:"Axion23",
+        imagen:require('../assets/objetos/montonRuedas.png'),
+        modelo:"Axion23",
+        tipo:"Invierno" ,
+        precio:40.89,
+    },
+    {
+        marca:"Axion23",
+        imagen:require('../assets/objetos/montonRuedas.png'),
+        modelo:"Axion23",
+        tipo:"Invierno" ,
+        precio:40.99,
+    },
     
 ]
 
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -3,23 +3,31 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Feather from 'react-native-vector-icons/Feather'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Svg from 'react-native-svg'
-
+import MapView, { Marker } from 'react-native-maps';
 import Constants from 'expo-constants'
 import { NeumaticosScreen } from ".."
 import { listaObjetos } from "../../constants/objetos"
+import { AirbnbRating  } from 'react-native-ratings';
 
+const LLAVE_IMAGE = require('../../assets/objetos/llave.png')
 
 const HomeScreen = () => {
-
+  const [region, setRegion] = useState({
+    latitude: 38.988266, 
+    longitude:  -1.861976,
+    latitudeDelta: 0.07,
+    longitudeDelta: 0.07,
+  });
+  
   const [contenido,setContenido ] = useState(true);
 
   return (
     <View style={styles.container}>
       <View style={styles.headerView}>
         <Image style={{margin:10,marginBottom:20,width:100,height:50}} source={require('../../assets/coche.png')} />
-        <Text>TallerIPO</Text>
+         <Text style={{fontSize:20}}>TallerIPO</Text>
         <View style={{flexDirection:'row',margin:20}}>
         <Ionicons name="cart-outline" size={35} style={{marginEnd:20}}/>
         <Feather name="user" size={30}/>
@@ -55,27 +63,61 @@ const HomeScreen = () => {
       {contenido ? 
       <View style ={{alignItems:'center'}}>
         <Image style={{width:'100%',height:235,marginHorizontal:5,marginBottom:20}} source={require('../../assets/homeImage.png')}/>
-        <Text style={{fontSize:20,marginBottom:10}}>Encuentranos en:</Text>
+        <Text style={{fontSize:20,marginBottom:10}}>Puedes encontrarnos en:</Text>
         <View style={{width:'100%',height:350,borderRadius:20,backgroundColor:'#FFB660',alignItems:'center'}}>
-          <Text style={{marginVertical:145}}>
-            PONER API CON MAPITA
-          </Text>
+          <MapView style={styles.map} initialRegion={region} region={region}>
+                <Marker
+                  coordinate={{
+                  latitude: 38.97873523731518,
+                  longitude:  -1.856899295429737
+                  }}
+                  title="TallerIPO"
+                  description="¡Aquí estamos!"
+                  
+                  />
+           </MapView>
+          
         </View>
         
       </View>
         :
        <View>
-        <FlatList data={listaObjetos}
+        <FlatList 
+        numColumns={2}
+        contentContainerStyle={{alignItems:'center'}}
+        data={listaObjetos}
         renderItem={({item})=>{
           return(
-            <View>
-              
-              <View>
-                <Text>Marca: {item.marca}</Text>
-                <Text>Modelo: {item.modelo}</Text>
-                <Text>Tipo: {item.tipo}</Text>
+           
+              <View style={styles.cardContainer}>
+
+                <Image style={{width:80,height:80,marginVertical:10}} source={item.imagen} /> 
+
+                <View style={styles.cardTitle}>
+                  <View style={{flexDirection:'row',alignItems:'center'}}>
+                    <View style={{marginBottom:10}}>  
+                      <Text style={{fontSize:12}}>Marca: {item.marca}</Text>
+                      <Text style={{fontSize:12}}>Modelo: {item.modelo}</Text>
+                      <Text style={{fontSize:12}}>Tipo: {item.tipo}</Text>
+                    </View>
+                    
+                    <Text style={{fontSize:16,fontWeight:'600',marginStart:17}}>{item.precio.toFixed(2)}€</Text>
+                  </View>
+
+                  <AirbnbRating 
+                  starImage={LLAVE_IMAGE}
+                  count={5}
+                  size={20}
+                  showRating={false}
+                  isDisabled={true}
+                  defaultRating={4}
+                  selectedColor={'#f1c40f'}
+                  />
+
+                </View>
               </View>
-            </View>
+            
+            
           )
         }} />
 
@@ -88,6 +130,9 @@ const HomeScreen = () => {
     </View>
   )
 }
+HomeScreen.defaultProps = {
+  markers: [],
+};
 
 export default HomeScreen
 
@@ -112,5 +157,24 @@ const styles = StyleSheet.create({
  seleccion:{
   backgroundColor:'#E9942F',
   
- }
+ }, 
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  //* Tarjetas
+  cardContainer:{
+    
+    alignItems:'center',
+    backgroundColor:'#D9D9D9',
+    borderRadius: 12,
+    margin:5
+  },
+  cardTitle:{
+    borderRadius:10,
+    padding:5,
+    backgroundColor:'white',
+  }
+
 })
